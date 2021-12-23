d985f9950b7467494516c7489f137b691013d3b0
Infowiste <47015938+sirineBH@users.noreply.github.com>
2021-12-23 22:00:53 +0100

    new dep

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express")
 const app = express();
+
 app.use(express.static(__dirname + '/app'));
+//app.use('/', express.static(__dirname + '/app'));
+
 app.listen(process.env.PORT || 3000);
